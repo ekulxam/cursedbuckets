841ae4645b679774a1c711248efa37defa31ec4a
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2024-02-09 19:52:18 -0800

    Add files via upload

---
+++ src/main/java/survivalblock/cursedbuckets/CursedBuckets.java
@@ -0,0 +1,25 @@
+package survivalblock.cursedbuckets;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import survivalblock.cursedbuckets.item.ModItems;
+import survivalblock.cursedbuckets.item.custom.AxolotlSunBucketItem;
+import survivalblock.cursedbuckets.item.custom.InfiniteBucketItem;
+
+import static survivalblock.cursedbuckets.item.ModItems.AXOLOTL_SUN_BUCKET;
+import static survivalblock.cursedbuckets.item.ModItems.BAVA_LUCKET;
+
+public class CursedBuckets implements ModInitializer {
+
+	public static final String MOD_ID = "cursedbuckets";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+		FuelRegistry.INSTANCE.add(BAVA_LUCKET, 1000);
+		FuelRegistry.INSTANCE.add(AXOLOTL_SUN_BUCKET, 4000);
+	}
+}
